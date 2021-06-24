65cfd3db049e605e2299f5e8c046370034b6c7fe
John <jzlowie@gmail.com>
2021-06-23 22:12:27 -0600

    LighthouseBnB Select assignment commit

---
+++ 1_queries/03_property_listings_by_city.sql
@@ -0,0 +1,12 @@
+SELECT 
+  properties.id AS id,
+  properties.title AS title,
+  properties.cost_per_night AS cost_per_night,
+  AVG(property_reviews.rating) AS average_rating
+FROM properties
+JOIN property_reviews ON property_id=properties.id
+WHERE city LIKE '%ancouv%'
+GROUP BY properties.id
+HAVING AVG(property_reviews.rating)>=4
+ORDER BY cost_per_night
+LIMIT 10;
